d92d41a91b1f633dcd271f6f2c691a8b048d8526
davidlouistandio <davidlouistandio@gmail.com>
2025-01-03 19:10:12 +0800

    Learn state and theme

--- app/src/main/java/com/example/uitoolkitcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/uitoolkitcompose/ui/theme/Theme.kt
@@ -9,28 +9,29 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val LightBlue = Color(0xFFD7EFFE)
+val Navy = Color(0xFF073042)
+val Chartreuse = Color(0xFFEFF7CF)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    primaryContainer = Navy,
+    onPrimaryContainer = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = PurpleGrey80,
+    tertiary = Pink40,
+    primaryContainer = LightBlue,
+    onPrimaryContainer = Navy
 )
 
 @Composable
